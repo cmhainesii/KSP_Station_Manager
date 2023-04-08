58ec8e3cdb590438f4f1762ed752f02bb0288cda
Charles Haines II <charlesmhainesii@gmail.com>
2023-04-08 15:50:13 -0400

    added delete station functionality

--- menu.cpp
+++ menu.cpp
@@ -10,7 +10,8 @@ string Menu::getMenuText()
     ss << "1) Load Stations from Disk" << std::endl;
     ss << "2) Write Stations to Disk" << std::endl;
     ss << "3) Add New Station" << std::endl;
-    ss << "4) List All Stations" << std::endl;
+    ss << "4) Delete Station" << std::endl;
+    ss << "5) List All Stations" << std::endl;
     ss << "q) Quit" << std::endl << std::endl;
 
     return ss.str();
